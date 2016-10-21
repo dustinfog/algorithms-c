0c2bb3129b73132604ad5169e17a455c9442ecea
fp-zhandongpan <qwer1234>
2016-10-21 09:28:10 +0800

    count beads

--- count_beads.h
+++ count_beads.h
@@ -20,6 +20,8 @@ extern "C" {
 
     /**
      * 一串首尾相连的珠子(m个)，有N种颜色(N<=10)
+     * 设计一个算法，取出其中一段，要求包含所有N中颜色，并使长度最短。
+     * 并分析时间复杂度与空间复杂度。
      * @param beads
      * @param beadNum
      * @param colorNum
